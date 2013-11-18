034c1d0399d77392acf89a69c6ec6ef4c2b1a185
alumno <alumno@2A5PC12>
2013-11-18 12:34:18 +0100

    Ver 1

--- web/index.js
+++ web/index.js
@@ -1,5 +1,15 @@
-var app = angular.module("app",[])
-app.controller("indexCtrl",function($scope){
-   $scope.nombre = "KiKe";
+var app = angular.module("app", [])
+app.controller("indexCtrl", function($scope, $http) {
+
+    $scope.busque = "Entidad Bancaria"
+    $http.get("http://localhost:8084/daIgual/entidadesBancarias.json.jsp").success(function(resultado) {
+        $scope.entidadesBancarias = resultado;
+    })
+    $scope.buscar = function() {
+        $http.get("http://localhost:8084/daIgual/entidadesBancarias.json.jsp?nombre="+$scope.busque).success(function(result) {
+            $scope.entidadesBancarias=result;
+        })
+    }  
+
 })
 
